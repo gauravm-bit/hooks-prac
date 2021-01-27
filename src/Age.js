57b929606e5eef4c4ea9fb369ba6fd93d9da8220
gauravm-bit <gauravwrk01@gmail.com>
2021-01-27 15:47:02 +0530

    ADDED: multiple states in react hooks

--- src/Age.js
+++ src/Age.js
@@ -3,14 +3,23 @@ import React,{useState} from 'react';
 const Age = () => {
 
     const[initialAge,setAge] = useState(12)
+    const[initialSibling,setSibling] = useState(3)
+
     const handleClick = () => {
         setAge(initialAge+1)
     }
 
+    const handleSiblingCount = () => {
+        setSibling(initialSibling+1)
+    } 
+ 
     return(
         <div>
             Your age is {initialAge}
             <button onClick={handleClick}>ADD AGE</button>
+            <br/>
+            You have {initialSibling} siblings
+            <button onClick={handleSiblingCount}> ADD SIBLINGgit</button>
         </div>
     )
 }
